3ba2fffd257169eeac0f6d21c34f21275845a6a6
Aditya Raj <adityarajofficial0096@gmail.com>
2024-06-05 11:30:27 +0530

    Imported various modules for typescript functions

---
+++ src/app/views/base/accordion/accordions.component.spec.ts
@@ -0,0 +1,9 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AccordionModule, CardModule, GridModule } from '@coreui/angular';
+import { IconSetService } from '@coreui/icons-angular';
+import { iconSubset } from '../../../icons/icon-subset';
+import { AccordionsComponent } from './accordions.component';
+
