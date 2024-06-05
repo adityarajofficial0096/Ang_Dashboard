e192d7fe663baaa3f74b52ba5e6c073c56ac2157
Aditya Raj <adityarajofficial0096@gmail.com>
2024-06-05 12:08:35 +0530

    Added Import module for various purpose used in function in typescript

---
+++ src/app/views/base/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { DocsExampleComponent } from '@docs-components/public-api';
+import {
+  BreadcrumbComponent,
+  BreadcrumbItemComponent,
+  BreadcrumbRouterComponent,
+  CardBodyComponent,
+  CardComponent,
+  CardHeaderComponent,
+  ColComponent,
+  RowComponent,
+  TextColorDirective
+} from '@coreui/angular';
