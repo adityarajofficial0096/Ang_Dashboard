f6458aa6a107af4b2ddf846853425cc315f4c7d4
Aditya Raj <adityarajofficial0096@gmail.com>
2024-06-12 18:32:05 +0530

    Removed unneccary files

--- docs/browser/chunk-INTNWTC6.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-ZG7TN3SX.js";var t=[{path:"",loadComponent:()=>import("./chunk-MLO6W33O.js").then(o=>o.DashboardComponent),data:{title:$localize`Dashboard`}}];export{t as routes};
--- docs/browser/chunk-XP7WKWAI.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-ZG7TN3SX.js";var o=[{path:"",loadComponent:()=>import("./chunk-27TJS5UO.js").then(t=>t.WidgetsComponent),data:{title:"Widgets"}}];export{o as routes};
